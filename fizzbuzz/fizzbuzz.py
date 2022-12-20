c75afb0d584d06a507232e492f6e39e2c72897fd
Antoine Biard <antoine.biard.10@gmail.com>
2022-12-20 14:36:54 +0100

    A bit of clean up

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -5,8 +5,10 @@ For the multiples of five print Buzz instead of the number
 For numbers which are multiples of both 3 and 5 print FizzBuzz instead of the number
 """
 
+
 def fizzbuzz(n: int):
     print(n)
+
 
 if __name__ == "__main__":
     fizzbuzz(100)
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -1,5 +1,6 @@
 def test_true():
     assert 1 == 1
 
+
 def test_false():
     assert 1 == 0
