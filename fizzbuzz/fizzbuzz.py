667e8dcea41558a24619ff43bc7e443e89fff38c
Antoine Biard <antoine.biard.10@gmail.com>
2022-12-20 14:12:38 +0100

    First kata: Fizzbuzz

---
+++ fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+"""
+Write a program that prints one line for each number from 1 to 100
+For multiples of three print Fizz instead of the number
+For the multiples of five print Buzz instead of the number
+For numbers which are multiples of both 3 and 5 print FizzBuzz instead of the number
+"""
+
+def fizzbuzz(n: int):
+    print(n)
+
+if __name__ == "__main__":
+    fizzbuzz(100)
